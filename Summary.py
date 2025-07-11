38e14751094b23778008f3c7af06322dac250f4d
grgor08 <y.abdelazizomer@gmail.com>
2025-07-11 16:01:43 -0500

    Generating N21700CGP results replicating the spec-sheet life test

--- Summary.py
+++ Summary.py
@@ -4,6 +4,6 @@
 import pybamm
 
 parameters = pybamm.LithiumIonParameters()
-parameter_values = pybamm.ParameterValues("Marquis2019")
+parameter_values = pybamm.ParameterValues("OKane2022")
 output_file = "lithium_ion_parameters.txt"
 parameter_values.print_parameters(parameters, output_file)
